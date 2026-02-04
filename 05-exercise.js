87a3a36155008737003ccfa866c998c8e6788bc3
Madan <29ramadhanirachman@gmail.com>
2026-02-04 10:40:34 +0700

    edit

--- 05-exercise.js
+++ 05-exercise.js
@@ -2,5 +2,20 @@ function split(kalimat) {
   let word = "";
   let result = [];
 
-  for (i = 0; i <= kalimat.length; i++) {}
+  for (i = 0; i <= kalimat.length - 1; i++) {
+    if (kalimat[i] != " ") {
+      word = word + kalimat[i];
+    } else {
+      result.push(word);
+      word = "";
+    }
+  }
+
+  if (word != "") {
+    result.push(word);
+  }
+
+  return result;
 }
+
+console.log(split("aku ngoding"));
